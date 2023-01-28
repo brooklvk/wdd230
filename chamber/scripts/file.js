10ce024a8dbf12610dce1cfe55d37e19ab44b421
brooklvk <brooklvk@byui.edu>
2023-01-28 16:16:46 -0700

    edited

--- chamber/scripts/file.js
+++ chamber/scripts/file.js
@@ -1,4 +1,12 @@
 
+// Date in header 
 const now = new Date();
 const fulldate = new Intl.DateTimeFormat("en-UK", { dateStyle: "full" }).format(now);
 document.querySelector("#date").textContent = fulldate;
+
+// Current year in footer 
+const year = new Date().getFullYear();
+document.querySelector("#currentYear").textContent = year;
+
+// Last Modified in footer 
+document.querySelector("#lastModified").innerHTML = document.lastModified;
