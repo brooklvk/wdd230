188e9e8ca83aad8d34fd09079e0447c3bc22e14b
brooklvk <brooklvk@byui.edu>
2023-01-30 19:52:26 -0700

    Revert "edited"
    This reverts commit d52472d8053c208beb1e23ee6d00dfae1d4fa91c.

--- chamber/scripts/file.js
+++ chamber/scripts/file.js
@@ -4,17 +4,6 @@ const now = new Date();
 const fulldate = new Intl.DateTimeFormat("en-UK", { dateStyle: "full" }).format(now);
 document.querySelector(".date").textContent = fulldate;
 
-//Responsive nav bar 
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-function myFunction() {
-    var x = document.getElementById(".myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
-
 // Current year in footer 
 const year = new Date().getFullYear();
 document.querySelector("#currentYear").textContent = year;
