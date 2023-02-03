fef780367bf9453a12d79b831640a760ef11285a
brooklvk <brooklvk@byui.edu>
2023-02-02 21:28:49 -0700

    edited

---
+++ lesson05/scripts/file.js
@@ -0,0 +1,19 @@
+
+const input = document.querySelector(input);
+
+const button = document.querySelector(button);
+button.addEventListener('click', function() {
+    if (input == "") {
+        document.querySelector(ul).createElement(li).textContent = input;
+        document.createElement(button).createAttribute(id = "delete").createAttribute(type = "button").textContent = "X";
+        // append the li element with the delete button
+        document.querySelector("#delete").createAttribute(onclick )
+        // append the list element with the li element just created and appended with text and the delete button
+        // add an event listener to the delete button that removes the li element when clicked
+        // send the focus to the input element
+        document.querySelector(input).textContent = "";
+    }
+})
+
+
+const list = document.querySelector("list");
