8fd158e23e1dfe42bc5cc8d99075397c634ce1ea
brooklvk <brooklvk@byui.edu>
2023-02-13 11:40:47 -0700

    edited

---
+++ chamber/scripts/windchill.js
@@ -0,0 +1,7 @@
+// get the two input values of temperature and wind speed,
+// check to make sure they meet the specification limits  (<=50°F and >3.0mph) allowed to officially calculate the wind chill, and
+// either calculate and display the wind chill factor value or display "N/A" (not applicable) if the input values did not meet the requirements.
+
+const temperature = document.querySelector("#temperature");
+const windSpeed = document.querySelector("#wind-speed");
+
