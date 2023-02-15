54938e5fbedc9eb10f5ba6d807fa9e887d576749
brooklvk <brooklvk@byui.edu>
2023-02-15 15:24:50 -0700

    edited wind chill

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -9,7 +9,7 @@ const windSpeed = parseFloat(document.querySelector("#wind-speed").textContent);
 let windChill = 0;
 
 if (temperature <= 50 && windSpeed > 3) {
-    windChill = 35.74 + 0.6215 * temperature - 35.75 * windSpeed ** 0.16 + 0.4275 * temperature * windSpeed ** 0.16;
+    windChill = calcWindChill(temperature, windSpeed);
 }
 
 else {
@@ -17,3 +17,8 @@ else {
 }
 
 document.querySelector("#wind-chill").textContent = windChill;
+
+function calcWindChill(temp, speed) {
+    const windChill = 35.74 + 0.6215 * temp - 35.75 * speed ** 0.16 + 0.4275 * temp * speed ** 0.16;
+    return windChill
+}
