4d6eb78a2a9fcd150abd40b0b217ee81b0509c1a
brooklvk <brooklvk@byui.edu>
2023-02-23 15:37:42 -0700

    edited

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,3 +1,4 @@
+
 // get the two input values of temperature and wind speed,
 // check to make sure they meet the specification limits  (<=50°F and >3.0mph) allowed to officially calculate the wind chill, and
 // either calculate and display the wind chill factor value or display "N/A" (not applicable) if the input values did not meet the requirements.
