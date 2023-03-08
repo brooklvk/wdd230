ca49650de20ee9e52f5908b72999f8e4cdf2e6a3
brooklvk <brooklvk@byui.edu>
2023-03-08 11:05:53 -0700

    edited

---
+++ chamber/scripts/weather-api.js
@@ -0,0 +1,29 @@
+
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');   
+
+const url = `{https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=f3e31394eb86ed958067f299ae42b4c7}`;
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data); // this is for testing the call
+        // displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  
+  apiFetch();
+
+function displayResults(weatherData) {
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+}
+
