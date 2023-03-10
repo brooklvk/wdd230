dd2be0e74cf7e59075550c341dd2b049c1ea90b2
brooklvk <brooklvk@byui.edu>
2023-03-10 12:08:21 -0700

    edited

--- chamber/scripts/weather-api.js
+++ chamber/scripts/weather-api.js
@@ -3,6 +3,7 @@ const currentTemp = document.querySelector('#temperature');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 const windSpeed = document.querySelector('#wind-speed');
+const windChill = document.querySelector('#wind-chill');
 
 const url = `https://api.openweathermap.org/data/2.5/weather?q=Manassa&units=imperial&appid=f3e31394eb86ed958067f299ae42b4c7`;
 
@@ -34,6 +35,12 @@ function displayResults(weatherData) {
     weatherIcon.setAttribute('src', iconsrc);
     weatherIcon.setAttribute('alt', desc);
     captionDesc.textContent = desc;
+
+    let windChill = "N/A";
+    if (temp <= 50 && speed > 3) {
+        windChill = (35.74 + 0.6215 * temp - 35.75 * speed ** 0.16 + 0.4275 * temp * speed ** 0.16);
+    }
+    document.querySelector("#wind-chill").textContent = windChill;
 }
 
 apiFetch();
