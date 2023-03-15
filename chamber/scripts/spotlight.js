27bcc9cde5a69d2f77005efff69772919d11109f
brooklvk <brooklvk@byui.edu>
2023-03-15 14:49:18 -0600

    edited spotlights

--- chamber/scripts/spotlight.js
+++ chamber/scripts/spotlight.js
@@ -10,7 +10,7 @@ async function getBusinessData() {
     displayBusinesses(data.businesses);
 }
 
-const displayBusinesses = (businesses) => {
+const disyBusses = (businesses) => {
     // const spotlights = document.querySelector("#spotlights");
 
     const spotlight1Name = document.querySelector("#spotlight1 h2");
@@ -22,16 +22,64 @@ const displayBusinesses = (businesses) => {
     const spotlight3Name = document.querySelector("#spotlight3 h2");
     const spotlight3Info = document.querySelector("#spotlight3 p");
 
-    let random = Math.floor(Math.random() * (10 - 1) + 1);
-
-    // choose a random business 
-    let randomBusiness = businesses[random];
+    chooseBusiness(businesses);
+    
 
     // add that business info to 1 
     spotlight1Name.innerHTML = randomBusiness.name;
-    spotlight1Info.innerHTML = 
+    spotlight1Info.innerHTML = `Phone: ${randomBusiness.phone}`;
 
     // same for 2 and 3 
+
+}
+
+const displayBusinesses = (businesses) => {
+    const cards = document.querySelector("#spotlights");
+
+    businesses.forEach((business) => {
+        let card = document.querySelector("#spotlight1");
+        let h2 = document.createElement("h2");
+        let address = document.createElement("p");
+        let phone = document.createElement("p");
+        let url = document.createElement("p");
+        let membership = document.createElement("p");
+        let image = document.createElement("img");
+
+        h2.textContent = `${business.name}`;
+        address.textContent = `${business.address}`;
+        phone.textContent = `${business.phone}`;
+        url.textContent = `${business.url}`;
+        membership.textContent = `Membership: ${business.membership}`;
+
+        image.setAttribute("src", business.img);
+        image.setAttribute("alt", business.alt);
+        image.setAttribute("loading", "lazy");
+        image.setAttribute("width", "340");
+        image.setAttribute("height", "auto");
+
+        card.appendChild(h2);
+        card.appendChild(address);
+        card.appendChild(phone);
+        card.appendChild(url);
+        card.appendChild(membership);
+        card.appendChild(image);
+  
+        cards.appendChild(card);
+    })
+}
+
+const chooseBusiness = (businesses) => {
+    // choose a random business 
+    let random = Math.floor(Math.random() * (9 - 1) + 1);
+    let randomBusiness = businesses[random];
+
+    if (randomBusiness.membership == "Gold" || randomBusiness.membership == "Silver") {
+        
+    }
+
+    else {
+        chooseBusiness(businesses);
+    }
 
 }
 
