ddc7ad3d6993f304f807caf206101263a0090dc5
brooklvk <brooklvk@byui.edu>
2023-03-16 14:54:03 -0600

    edited visits

--- chamber/scripts/visits.js
+++ chamber/scripts/visits.js
@@ -1,11 +1,32 @@
 
-const now = new Date();
+// const now = new Date();
 
+// // Visit message 
+// let visit = now;
+// localStorage.setItem("numberOfVisits", visit);
+
+// document.querySelector("#visit").innerHTML = visit;
+
+// //localStorage.setItem("numberOfVisits", 0);
+
+
+const visitElement = document.querySelector("#visit");
 // Visit message 
-let visit = now;
-localStorage.setItem("numberOfVisits", visit);
+// let visits = now.getMinutes();
+localStorage.setItem("numberOfVisits", numberOfVisits);
 
-document.querySelector("#visit").innerHTML = visit;
+// let numberOfVisits = localStorage.getItem("numberOfVisits");
+// let difference = now.getMinutes() - visit;
+// localStorage.setItem("difference", difference);
 
+if (!numberOfVisits) {
+    numberOfVisits = 0;
+  }
+document.querySelector("#visit").innerHTML = difference;
+
+numberOfVisits = +numberOfVisits + 1; 
 //localStorage.setItem("numberOfVisits", 0);
 
+localStorage.setItem("numberOfVisits", numberOfVisits);
+
+visitElement.innerHTML = numberOfVisits;
